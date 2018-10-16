d141865f0dae6a43a78cdd3af3c6bc1491037f02
vithuvijay <spidyv3@gmail.com>
2018-10-16 17:55:31 +0530

    uki react auth

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card, CardTitle } from 'material-ui/Card';
+
+
+const HomePage = () => (
+  <Card className="container">
+    <CardTitle title="React Application" subtitle="This is the home page of Vithu" />
+  </Card>
+);
+
+export default HomePage;
